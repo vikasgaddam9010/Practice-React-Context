71fbc93278e44ee54002c27878a85710874c21f8
vikasgaddam9010 <vikasgaddam9010@gmail.com>
2024-04-23 09:29:01 +0000

    React COntext

---
+++ src/components/FeaturesSection/index.js
@@ -0,0 +1,18 @@
+import Playtime from '../Playtime'
+import NewWaysToConnect from '../NewWaysToConnect'
+
+import LanguageContext from '../../LanguageContext'
+
+import './index.css'
+
+const FeaturesSection = props => {
+  const {activeLanguage} = props
+  return (
+    <div className="features-section-container">
+      <Playtime activeLanguage={activeLanguage} />
+      <NewWaysToConnect activeLanguage={activeLanguage} />
+    </div>
+  )
+}
+
+export default FeaturesSection
